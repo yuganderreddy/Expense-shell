680241cadf5d7fdd0261f2bbfd900a734db1b221
YuganderReddy <yugander243@gmail.com>
2024-09-17 21:43:08 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -6,11 +6,11 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-dnf list installed mysql
-if [ $? -ne 0 ]
-then
-    echo -e "$R mysql is not installed, going to install...$N"
-    dnf install mysql -y
-else
-    echo -e "$G mysql is already installed, no worries...$N"
-fi
+dnf install mysql-server -y
+# if [ $? -ne 0 ]
+# then
+#     echo -e "$R mysql is not installed, going to install...$N"
+#     dnf install mysql-server -y
+# else
+#     echo -e "$G mysql is already installed, no worries...$N"
+# fi
