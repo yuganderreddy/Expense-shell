29621ee47b3286f43b5fa6038f12e3aebf296dc7
YuganderReddy <yugander243@gmail.com>
2024-09-19 00:29:05 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
-dnf install mysql-server -y
+#dnf install mysql-server -y
 # if [ $? -ne 0 ]
 # then
 #     echo -e "$R mysql is not installed, going to install...$N"
@@ -14,3 +14,44 @@ dnf install mysql-server -y
 # else
 #     echo -e "$G mysql is already installed, no worries...$N"
 # fi
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld
+VALIDATE $? "Starting MySQL Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting up root password for MySQL Server"
