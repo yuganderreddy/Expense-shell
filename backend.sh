dd91ba72fd5bc9782d8d3f7734260ead327beb91
YuganderReddy <yugander243@gmail.com>
2024-09-21 16:45:18 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -61,6 +61,6 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "downloaded the backend application code to temp directory"
 
 cd /app
-
+rm -rf /app/*  #remove the existing code
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting the backend application code to temp directory"
